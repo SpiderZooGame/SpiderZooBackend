356eea41a137073d70a01eadaff65665ebc233a8
Siphesihle1415 <siphesihle.mthethwa@bbd.co.za>
2024-04-30 13:44:45 +0200

    Changed db instance class

--- terraform/database.tf
+++ terraform/database.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "virtual_spider_zoo_db" {
   storage_type                = "gp2"
   engine                      = "postgres"
   engine_version              = "15.6"
-  instance_class              = "db.t2.micro"
+  instance_class              = "db.t3.micro"
   db_subnet_group_name        = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids      = [aws_security_group.allow_tcp.id]
   maintenance_window          = "Mon:00:00-Mon:01:00"
