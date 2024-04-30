388d59bbfd5f749c5261bba2db95e5184784d37c
Siphesihle1415 <siphesihle.mthethwa@bbd.co.za>
2024-04-30 13:04:45 +0200

    changed db config kms key

--- terraform/database.tf
+++ terraform/database.tf
@@ -27,22 +27,22 @@ resource "aws_vpc_security_group_egress_rule" "allow_tcp" {
 #########################################
 # RDS Database
 resource "aws_db_instance" "virtual_spider_zoo_db" {
-  identifier                    = var.db_name
-  username                      = var.db_username
-  allocated_storage             = 20
-  storage_type                  = "gp2"
-  engine                        = "postgres"
-  engine_version                = "16.1"
-  instance_class                = "db.t3.micro"
-  db_subnet_group_name          = aws_db_subnet_group.db_subnet_group.name
-  vpc_security_group_ids        = [aws_security_group.allow_tcp.id]
-  maintenance_window            = "Mon:00:00-Mon:01:00"
-  publicly_accessible           = true
-  skip_final_snapshot           = true
-  manage_master_user_password   = true
-  multi_az                      = false
-  backup_retention_period       = 0
-  master_user_secret_kms_key_id = var.kms_key_id
+  identifier                  = var.db_name
+  username                    = var.db_username
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "16.1"
+  instance_class              = "db.t3.micro"
+  db_subnet_group_name        = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids      = [aws_security_group.allow_tcp.id]
+  maintenance_window          = "Mon:00:00-Mon:01:00"
+  publicly_accessible         = true
+  skip_final_snapshot         = true
+  manage_master_user_password = true
+  multi_az                    = false
+  backup_retention_period     = 0
+  kms_key_id                  = var.kms_key_id
 
   tags = var.common_tags
 }
