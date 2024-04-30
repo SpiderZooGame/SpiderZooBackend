047624be73c3fe2db63ea5424a061fdc3417b6ff
Siphesihle1415 <siphesihle.mthethwa@bbd.co.za>
2024-04-30 13:22:51 +0200

    Changed db engine version

--- terraform/database.tf
+++ terraform/database.tf
@@ -32,7 +32,7 @@ resource "aws_db_instance" "virtual_spider_zoo_db" {
   allocated_storage           = 20
   storage_type                = "gp2"
   engine                      = "postgres"
-  engine_version              = "12.4"
+  engine_version              = "15.6"
   instance_class              = "db.t2.micro"
   db_subnet_group_name        = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids      = [aws_security_group.allow_tcp.id]
